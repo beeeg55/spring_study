119e2fcce11dcb27f301d6a797aab5ef3d65bdac
beeeg55 <beeeg@naver.com>
2024-05-19 23:50:54 +0900

    multi module project practice

---
+++ SpringProject/workspace/multimodule/module-api/src/main/java/dev/be/moduleapi/service/DemoService.java
@@ -0,0 +1,14 @@
+package dev.be.moduleapi.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DemoService {
+    public String save(){
+        return "save";
+    }
+    public String find(){
+        return "find";
+    }
+}
+
